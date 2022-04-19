6e6a3cc535affdba1a6a140a0158727bb72bc64e
Kim <a75497461@gmail.com>
2022-04-19 22:56:19 +0900

    hello-spring JDBC

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -41,6 +41,7 @@ public class MemoryMemberRepositoryTest {
         member2.setName("spring2");
         repository.save(member2);
 
+
         Member result = repository.findByName("spring1").get();
 
         assertThat(result).isEqualTo(member1);
